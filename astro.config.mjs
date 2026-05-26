7b30a74920623b52ba71154b822a20a9106d7623
Tyler Kendrick <145080887+Tyler-R-Kendrick@users.noreply.github.com>
2026-05-26 14:20:36 -0500

    Set up GitHub Pages deployment
    Configure Astro and GitHub Actions for GitHub Pages deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://tyler-r-kendrick.github.io',
+  base: '/tk-technews',
   markdown: {
     shikiConfig: {
       theme: 'github-light'
